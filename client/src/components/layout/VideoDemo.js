95e696b27062c1f50a14e27da48dbcb900e11921
Jim Velasco <jim.velasco@gmail.com>
2018-06-22 16:48:10 -0600

    trying html5 video widget

--- client/src/components/layout/VideoDemo.js
+++ client/src/components/layout/VideoDemo.js
@@ -3,11 +3,19 @@ import video from "../../img/MimeDemo3.mp4";
 
 class VideoDemo extends Component {
   render() {
+    // let vstr =
+    //   "https://s3-us-west-2.amazonaws.com/mirror-thumbnails/MimeDemo3.mp4";
+
+    // let vstr =
+    //   "https://drive.google.com/open?id=1IYyP-TVCYGsxGACyjzAWsTMepoIoJiG_/view";
     return (
       <div className="centerdiv">
-        <iframe className="embed-responsive-item videoarea" src={video} />
+        {/* <iframe className="embed-responsive-item videoarea" src={video} /> */}
+        <video src={video} controls />
       </div>
     );
   }
 }
 export default VideoDemo;
+//https://drive.google.com/open?id=1IYyP-TVCYGsxGACyjzAWsTMepoIoJiG_
+//radiant-plateau-48717
